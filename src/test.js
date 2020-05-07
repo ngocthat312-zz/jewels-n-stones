478d75de94e1877cf4f9756ff8ad5a8e0e094402
That Nguyen <nguyenngocthat.2009@gmail.com>
2020-05-07 15:47:48 +0700

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,13 +1,12 @@
 const validate = string =>
 	string.replace(/ /g, '')
 		.split('')
-		.filter(function (item, pos, self) {
+		.filter((item, pos, self) => {
 			return self.indexOf(item) == pos;
 		})
 		.join('');
 
 const solution = (jewels, stones) => {
-	console.log(validate(jewels));
 	const result = stones.match(new RegExp(`[${validate(jewels)}]`, 'g')) || [];
 	return result.length;
 }
